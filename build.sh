6f2f3676c668a4cb7adb8f5def55718b406f8383
co232 <co232@node0.newgpu.cascadefuse-pg0.wisc.cloudlab.us>
2024-09-14 16:39:39 -0500

    Update make files

--- build.sh
+++ build.sh
@@ -1,8 +1,8 @@
 # Install BLAS
 # sudo apt-get install libblas-dev liblapack-dev
-# sudo apt-get install swig3.0
-# sudo ln -s /usr/bin/swig3.0 /usr/bin/swig
+# sudo apt-get install swig
 # pip install -r requirements.txt 
+# sudo apt-get install doxygen
 
 if [ -z "$1" ]; then
     build_path="build"
@@ -12,37 +12,34 @@ else
 fi
 
 
-if [ -z $FAISS_INSTALL_PREFIX ]; then
-    install_prefix="/users/co232/opt-dev"
-else
-    install_prefix=$FAISS_INSTALL_PREFIX
-fi
+# if [ -z $FAISS_INSTALL_PREFIX ]; then
+install_prefix="/users/co232/opt-dev"
+# else
+#     install_prefix=$FAISS_INSTALL_PREFIX
+# fi
 
 if [ -z $CUDAToolKitRoot ]; then
-    cudatoolkit_dir="/usr/local/cuda-12.3"
+    cudatoolkit_dir="/usr/local/cuda-12.2"
 else
     cudatoolkit_dir=$CUDAToolKitRoot
 fi
 
 # More about the flags setting checkout https://github.com/facebookresearch/faiss/blob/main/INSTALL.md 
-cmake_defs="-DCMAKE_BUILD_TYPE=${build_type} -DCMAKE_EXPORT_COMPILE_COMMANDS=ON -DCMAKE_INSTALL_PREFIX=${install_prefix} -DFAISS_ENABLE_GPU=ON -DFAISS_ENABLE_PYTHON=ON -DFAISS_ENABLE_RAFT=OFF -DBUILD_TESTING=ON -DBUILD_SHARED_LIBS=ON -DFAISS_ENABLE_C_API=ON -DCUDAToolkit_ROOT=${cudatoolkit_dir} -DCMAKE_CUDA_ARCHITECTURES=75;72"
+cmake_defs="-DCMAKE_BUILD_TYPE=${build_type} -DCMAKE_EXPORT_COMPILE_COMMANDS=ON -DCMAKE_INSTALL_PREFIX=${install_prefix} -DFAISS_ENABLE_GPU=ON -DFAISS_ENABLE_PYTHON=OFF -DFAISS_ENABLE_RAFT=OFF -DBUILD_TESTING=OFF -DBUILD_SHARED_LIBS=ON -DFAISS_ENABLE_C_API=ON -DCUDAToolkit_ROOT=${cudatoolkit_dir} -DCMAKE_CUDA_ARCHITECTURES=75;72"
 
 
-if [[ $2 == "USE_VERBS_API" ]]; then
-    cmake_defs="${cmake_defs} -DUSE_VERBS_API=1"
-fi
 
 # begin building...
 rm -rf ${install_prefix}/include/faiss ${install_prefix}/lib/libfaiss* ${install_prefix}/lib/cmake/faiss
 rm -rf ${build_path} 2>/dev/null
 mkdir ${build_path}
 cd ${build_path}
-cmake ${cmake_defs} ..
+cmake ..
 NPROC=`nproc`
 if [ $NPROC -lt 2 ]; then
     NPROC=2
 fi
-make -j faiss
+make faiss -j `expr $NPROC - 1` 2>err.log
 make install
 
 # Building python bindings
